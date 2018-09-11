25c59f24b2b5909f65bde06162ade7b6a80daccb
Adwin <23533932+dev-gitXplor@users.noreply.github.com>
2018-09-11 10:10:05 +0100

    Main starting point

--- Pong/main.cpp
+++ Pong/main.cpp
@@ -3,36 +3,8 @@
 
 int main()
 {
-	int width = 1000,
-		height = 600;
-	//sf::RenderWindow window(sf::VideoMode(width, height), "Pong", sf::Style::Titlebar);
-	//Player player(sf::Vector2f(0, 300));
+	int width = 1000, height = 600;
 	Game game = Game(Window(width, height, "Pong"));
 	game.Go();
-	//while (window.isOpen())
-	//{
-	//	//Update logic
-	//	sf::Event evnt;
-	//	while (window.pollEvent(evnt))
-	//	{
-	//		switch (evnt.type)
-	//		{
-	//		case sf::Event::Closed:
-	//			window.close();
-	//			break;
-	//		}
-	//	}
-	//	if (sf::Event::KeyPressed)
-	//	{
-	//		sf::Vector2f delta;
-	//		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-	//		{
-	//			delta = sf::Vector2f(0.0f, -1.0f);
-	//		}else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-	//		{
-	//			delta = sf::Vector2f(0.0f, 1.0f);
-	//		}
-	//		player.Move(delta);
-	//	}
 	return 0;
 }
